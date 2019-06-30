dedf6319c31f530970806ffbd9b0d2d02d636154
jnapoli <jnapoli@stanford.edu>
2019-06-29 17:01:24 -0700

    Finish predict script

--- autoencoda/predict.py
+++ autoencoda/predict.py
@@ -1,23 +1,50 @@
 import argparse
 import librosa
-import os
+import logging
 
-import preprocess
+import numpy as np
+import os.path as path
+import tensorflow as tf
 
-from keras.models import model_from_json
+from tensorflow.keras.models import load_model
+
+
+def print_result(result, thresh):
+    """Prints output message according to the result and the
+       prediction threshold.
+    """
+    if result >= thresh:
+        print('Congrats, it looks like you have a hit! ' + \
+              'This track is a hit with {:.2f} probability.'.format(result))
+    else:
+        print("Sorry, this doesn't look like a hit. Better luck next time!")
+    return None
 
 
 def main(args):
-    assert os.path.exists(args.path_track),
-           "Can't find your mp3 file! Please provide a valid path."
-    assert args.path_track[-3:] == 'mp3',
-           'Please provide the audio file in mp3 format.'
-    audio, sr = librosa.load(args.path_track)
-    sg = librosa.feature.melspectrogram(y=audio, sr=sr)
-
+    path_full_self = path.realpath(__file__)
+    path_base_self = path.dirname(path_full_self)
+    path_model = path.join(path_base_self, '..', 'model', 'model.h5')
+    if not path.exists(path_model):
+        raise IOError('Model file could not be located.')
+    if not path.exists(args.path_track):
+        raise IOError("Can't find your mp3 file! Please provide a valid path to an mp3.")
+    if not args.path_track.endswith('.mp3'):
+        raise ValueError('I expect an mp3 file.')
+    else:
+        model = load_model(path_model)
+        audio, sr = librosa.load(args.path_track)
+        sg = np.array([librosa.feature.melspectrogram(y=audio, sr=sr).mean(axis=1)])
+        X = sg / sg.max()
+        result = model.predict(X)[0][0]
+        print_result(result, args.threshold)
 
 
 if __name__ == '__main__':
+    # Take care of future deprecation warning
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+    # Parse arguments
     parser = argparse.ArgumentParser(
         description='Predict the popularity of your track!'
     )
@@ -25,5 +52,12 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help='mp3 file containing your music.')
+    parser.add_argument('--threshold',
+                        type=float,
+                        required=False,
+                        default=0.5,
+                        help='Probability threshold to use for inference.')
     args = parser.parse_args()
+
+    # Run inference
     main(args)
